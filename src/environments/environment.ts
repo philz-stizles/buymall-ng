af644750848743f1fc72bdc263e9c2ecbc5850cd
Theophilus Ighalo <theophilusighalo@gmail.com>
2023-12-27 13:52:08 +0100

    remove keys from env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   authBaseUrl:
-    'https://identitytoolkit.googleapis.com/v1/accounts:[END_POINT]?key=AIzaSyDRsXcf-_W-ej2sKPXVFTjX0dcZa9HqQgE',
+    'https://identitytoolkit.googleapis.com/v1/accounts:[END_POINT]?key=',
   baseUrl: 'https://buyemall-default-rtdb.firebaseio.com',
   encryptionKey: 'AIzaSyDRsXcf-',
 };
